e5ac74fe665ce9398e791b0ff599a2bb20c09682
asif279 <asifofficial8130@gmail.com>
2022-12-08 11:44:48 +0600

    signinout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import firebase from 'firebase/compat/app';
+import { initializeApp } from 'firebase/app';
+
+import 'firebase/compat/auth';
+import { } from 'firebase/analytics';
+import { } from 'firebase/firestore';
+import firebaseConfig from './firebase.config';
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+  const [user,setUser]= useState({
+  signedIn:false,
+  name:'',
+  email:'',
+  photo:''
+
+  });
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleClick = ()=>{
+    firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      const {displayName,photoURL,email}=result.user;
+      const signedUser={
+        signedIn:true,
+        name:displayName,
+        email:photoURL,
+        photo:email
+      }
+      setUser(signedUser);
+      //console.log(displayName,photoURL,email);
+    })
+    .catch((error) => {
+       var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+   
+    });
+    } 
+   // The firebase.auth.AuthCredential type that was used.
+   const handleSignOut =()=>{
+    firebase.auth().signOut()
+    .then((result) => {
+      const signedOutuser= {
+        signedIn:false,
+name:'',
+email:'',
+photo:''
+
+      }
+      setUser(signedOutuser);
+    }).catch((error) => {
+      // An error happened.
+    }); 
+      // ...
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+     {
+      user.signedIn?<button onClick={handleSignOut}>Signed Out</button>:<button onClick={handleClick}>Signed In</button>
+     }
+     {
+     user.signedIn && <p>Welcome , {user.name}</p>
+     }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCiimFZHNFoxx6jCiQt46vs5T96o5Q8Pu8",
+    authDomain: "ema-john-simple-as.firebaseapp.com",
+    projectId: "ema-john-simple-as",
+    storageBucket: "ema-john-simple-as.appspot.com",
+    messagingSenderId: "911384189016",
+    appId: "1:911384189016:web:3ad2566b4b73795f60522d",
+    measurementId: "G-N26XEX5CKH"
+  };
+
+  export default firebaseConfig;
