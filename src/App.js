58a1beb85c33b57b5596d6f22329e6b352b06b86
asif279 <asifofficial8130@gmail.com>
2022-12-15 13:07:56 +0600

    auth

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
 
   });
   const provider = new firebase.auth.GoogleAuthProvider();
+ const fbProvider = new firebase.auth.FacebookAuthProvider();
   const handleClick = ()=>{
     firebase.auth()
     .signInWithPopup(provider)
@@ -44,7 +45,41 @@ function App() {
    
     });
     } 
+
    // The firebase.auth.AuthCredential type that was used.
+   const handleFbsign =()=>{
+    firebase
+  .auth()
+  .signInWithPopup(fbProvider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    const credential = result.credential;
+    console.log(credential);
+
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user);
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    const accessToken = credential.accessToken;
+    console.log(accessToken);
+
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    const credential = error.credential;
+    console.log(error,errorCode,errorMessage,email,credential)
+    
+
+    // ...
+  });
+   }
    const handleSignOut =()=>{
     firebase.auth().signOut()
     .then((result) => {
@@ -142,6 +177,9 @@ success:false
      {
       user.signedIn?<button onClick={handleSignOut}>Signed Out</button>:<button onClick={handleClick}>Signed In</button>
      }
+     <br/>
+     <br/>
+     <button onClick={handleFbsign}>FB login</button>
      {
      user.signedIn && <p>Welcome , {user.name}
      ,{user.email}
